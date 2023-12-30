18a960fef6c7f5779ade3a7b0b2bbf7ba69dc6f6
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2023-12-30 20:36:44 +0500

    chore: update basic info

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,43 +20,43 @@ import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { XIcon } from "@/components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Ahmed Rizwan",
+  initials: "AR",
+  location: "Islamabad, Pakistan",
+  locationLink: "https://www.google.com/maps/place/Islamabad",
   about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+    "Lead Front-end Engineer focused on building products with extra attention to details",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://avatars.githubusercontent.com/u/4357275?v=4",
+  personalWebsiteUrl: "https://ahmedrizwan.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "ahmedrizwan@outlook.com",
+    tel: "+923465379140",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/ahmedrizwan",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/ahmedrz/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/sudo_rizwan",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Comsats Institute of Information Technology",
+      degree: "Bachelor's Degree in Computer Science",
+      start: "2010",
+      end: "2014",
     },
   ],
   work: [
